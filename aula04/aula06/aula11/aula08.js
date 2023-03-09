56f8e0722e6e84e0a5e86fcffda9224368cf89cf
JaymeManhica <107038187+JaymeManhica@users.noreply.github.com>
2023-03-09 17:58:57 +0200

    Initial commit

---
+++ aula04/aula06/aula11/aula08.js
@@ -0,0 +1,7 @@
+var vel = 12.5
+console.log(` a velocidade eh de 60 ${vel}km/h`)
+if (vel > 60) {
+    console.log(` Ultrapassou a velocidade permitida. MULTIDA!`)
+    console.log(``)
+}
+    console.log(`Dirija usando sempre o cinto de segunça`)
---
+++ aula04/aula12ex/script.js
@@ -0,0 +1,26 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 19
+    msg.innerHTML = `Agora sao ${hora} horas. `
+
+    if( hora >= 0 && hora <12){
+        // bom dia
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#a7ac2b'
+    } else if( hora >= 12 && hora < 18){
+        // boa tarde
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#ad827c'
+    } else{
+        // boa noite
+        img.src = 'imagens/noite1.png'
+        document.body.style.background = '#3a3a54'
+
+    }
+
+        
+
+}
